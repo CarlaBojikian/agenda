2f8715fa385fb7e66a8eb4206a821aaff8b9e460
Carla Bojikian <carla.moreno@hotmail.com>
2019-07-18 16:32:43 -0300

    Increase events, desctriptions, dates and usernames to acess them

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'date_creation')
+    list_filter = ('title', 'date', 'user_name',)
+
+admin.site.register(Evento, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Evento(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=100)
+    description = models.TextField(verbose_name='Description', blank=True, null=True)
+    date = models.DateTimeField(verbose_name='Event Date')
+    date_creation = models.DateTimeField(verbose_name='Creation Date', auto_now=True)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'  # Esta class vai fazer com que o arquivo se chame 'evento', senão o sistema nomeia de acordo um padrão pré-definido
+
+    def __str__(self):
+        return self.title
